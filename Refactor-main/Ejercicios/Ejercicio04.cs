9809d2da7380ce3a7ccdf3e610c16cec14af98f7
GiulianoMauroDana <104393352+GiulianoMauroDana@users.noreply.github.com>
2022-09-06 20:58:50 -0300

    Update Ejercicio04.cs

--- Refactor-main/Ejercicios/Ejercicio04.cs
+++ Refactor-main/Ejercicios/Ejercicio04.cs
@@ -5,22 +5,15 @@ using UnityEngine;
 
 public class Ejercicio04 : MonoBehaviour
 {
-    string BuscarEnemigos(string[] enemigos)
+     string BuscarEnemigos(string[] enemigos)
     {
         for (int i = 0; i < enemigos.Length; i++)
         {
-            if (enemigos[i].Equals("Araña"))
+            if (enemigos[i] != null)
             {
-                return "Araña";
+                return enemigos[i] ;
             }
-            if (enemigos[i].Equals("Lobo"))
-            {
-                return "Lobo";
-            }
-            if (enemigos[i].Equals("Orco"))
-            {
-                return "Orco";
-            }
+           
         }
         return String.Empty;
     }
