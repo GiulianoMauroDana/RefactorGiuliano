4f147448bc41116373426561547332f0e71fe5f2
GiulianoMauroDana <104393352+GiulianoMauroDana@users.noreply.github.com>
2022-09-06 20:40:02 -0300

    Update Ejercicio02.cs

--- Refactor-main/Ejercicios/Ejercicio02.cs
+++ Refactor-main/Ejercicios/Ejercicio02.cs
@@ -7,20 +7,11 @@ public class Ejercicio02 : MonoBehaviour
     int fuerza = 100;
     int agilidad = 50;
 
-
-    private void Start()
-    {
-        
-    }
-
-    private void Update()
-    {
-        
-    }
-
     private void AtaqueEspecial()
     {
-        if ((fuerza * 2) / 5 > -1 && (agilidad / 2 > 20))
+        int fuerzaEspecial = ((fuerza * 2) / 5)
+        int agilidadEspecial = agilidad / 2
+        if (fuerzaEspecial > -1 && (agilidadEspecial > 20))
 
         {
             Debug.Log("Ataque especial");
